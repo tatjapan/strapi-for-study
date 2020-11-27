e74e8fd4f252ea0c45ad56d5a4a723f66eda138c
tatjapan <tatsuya.tsuri@gmail.com>
2020-11-27 16:06:01 +0900

    Add some Validations

--- api/post/controllers/post.js
+++ api/post/controllers/post.js
@@ -1,8 +1,28 @@
-'use strict';
+"use strict";
+const { parseMultipartData, sanitizeEntity } = require("strapi-utils");
 
-/**
- * Read the documentation (https://strapi.io/documentation/v3.x/concepts/controllers.html#core-controllers)
- * to customize this controller
- */
-
-module.exports = {};
+module.exports = {
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      if (!data || !data.description) {
+        ctx.throw(400, "Please add some content");
+      }
+      if (!files || !files.length) {
+        ctx.throw(400, "Please add at least 1 file.");
+      }
+      entity = await strapi.services.post.create(
+        { ...data, likes: 0 },
+        { files }
+      );
+    } else {
+      ctx.throw(400, "You must submit a multipart request.");
+      //   entity = await strapi.services.post.create({
+      //     ...ctx.request.body,
+      //     likes: 0,
+      //   });
+    }
+    return sanitizeEntity(entity, { model: strapi.models.post });
+  },
+};
